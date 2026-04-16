8fafd0a335e4aadbd6eac032ba9ca0bb676f528e
johanamareliestradaromero4-tech <johanamareliestradaromero4@gmail.com>
2026-04-16 17:56:51 -0600

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,13 +1,3 @@
-// detectar clicks en el menú
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", function(e) {
-    e.preventDefault(); // evita recargar
-
-    const seccion = this.getAttribute("data-seccion");
-    mostrar(seccion);
-  });
-});
-
 function mostrar(seccion) {
   document.querySelectorAll(".contenido").forEach(sec => {
     sec.style.display = "none";
